ddd392a9666bdb212e01be5a4715267ec1779876
Changhwa Oh <changhwaoh.co@gmail.com>
2013-08-01 13:48:20 +0900

    setting: logging

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -73,9 +73,9 @@ environments {
 log4j = {
     // Example of changing the log pattern for the default console appender:
     //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
+    appenders {
+        console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+    }
 
     error  'org.codehaus.groovy.grails.web.servlet',        // controllers
            'org.codehaus.groovy.grails.web.pages',          // GSP
@@ -88,4 +88,5 @@ log4j = {
            'org.springframework',
            'org.hibernate',
            'net.sf.ehcache.hibernate'
+	debug  'net.withmate'									// Logging
 }
