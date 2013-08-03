24f2725e4e5bea974b35a8cc21e106e8d7e6f511
Changhwa Oh <changhwaoh.co@gmail.com>
2013-08-03 12:08:20 +0900

    fixed #33 to Bootstrap init data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,10 +5,10 @@ class BootStrap {
     def init = { servletContext ->
         if(!Users.count()){
 
-            new Users(email: "test1@narratage.com",name: "test1",passwd: "test1!").save(failOnError: true)
-            new Users(email: "test2@narratage.com",name: "test2",passwd: "test2!").save(failOnError: true)
-            new Users(email: "test3@narratage.com",name: "test3",passwd: "test3!").save(failOnError: true)
-            new Users(email: "test4@narratage.com",name: "test4",passwd: "test4!").save(failOnError: true)
+            new Users(email: "test1@narratage.com",name: "test1",passwd: "test1234!").save(failOnError: true)
+            new Users(email: "test2@narratage.com",name: "test2",passwd: "test2234!").save(failOnError: true)
+            new Users(email: "test3@narratage.com",name: "test3",passwd: "test3234!").save(failOnError: true)
+            new Users(email: "test4@narratage.com",name: "test4",passwd: "test4234!").save(failOnError: true)
         }
 
     }
